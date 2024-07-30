26c261c6354f8d2748af0c5df2c9b3ba8c188b68
Gaurav Sharma <126150677+iamgauravvvv@users.noreply.github.com>
2024-07-30 08:19:27 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function onMenuToggle(e) {
+  const navlinks = document.querySelector(".navLinks");
+  e.name = e.name === "menu" ? "close" : "menu";
+  navlinks.classList.toggle("left-[0%]");
+}
+
+AOS.init();
+
+const navDialog = document.getElementById('nav_dialog');
+
+function handleMenu(){
+ navDialog.classList.toggle('hidden');
+}
